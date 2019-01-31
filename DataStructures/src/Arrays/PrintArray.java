b9f76730bd2a782b562e7b1389097896cd432515
skandimalla <skandimalla@c02sgg64g8wp>
2019-01-30 21:41:57 -0800

    01/30 Commits

---
+++ DataStructures/src/Arrays/PrintArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		int arr[] = {3,4,5,6,7};
+		System.out.println("Printing Array Elements:");
+		for(int i=0; i<arr.length;i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+
+}
